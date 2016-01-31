de2e302a6a11365dee9ff6001b0f3877846e81d1
Cubb <danpopa1998@gmail.com>
2016-01-31 20:49:33 +0200

    genereaza midi

---
+++ main.py
@@ -0,0 +1,16 @@
+
+from mingus.midi.midi_file_out import write_Bar
+from mingus.containers import NoteContainer
+from mingus.containers import Bar
+import mingus
+
+
+
+b = Bar()
+b.place_notes(["A-4","G-4"], 4)
+
+b.place_notes("C-5", 4)
+
+b.place_notes(["E-5", "G-5", "A-5"], 2)
+
+write_Bar("test.mid",b,bpm=120)
