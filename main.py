ed77fd6e5cee038b9c61d76a228211513ea30d85
cubbic <danpopa1998@gmail.com>
2016-02-28 15:53:52 +0200

    arata leads_to[i] la nota corecta

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+# Apparently these import statements does not work in python 3
 import mingus.midi.midi_file_out as midi
 from mingus.containers import NoteContainer ,Track,Bar ,Composition,Suite
 from mingus.containers.instrument import MidiInstrument
@@ -9,71 +9,71 @@ import mingus ,random
 
 notes = ['C','D','E','F','G','A','B']
 class GeneratedTrack():
-	leads_to = [
-		[0,1,2,3,4,5,6] , 
-		[0,4,6],
-		[0,3,5],
-		[0,1,4,6],
-		[0,5],
-		[0,1,2,3,4],
-		[0]
-	]
+    leads_to = [
+        [0,1,2,3,4,5,6] , 
+        [0,4,6],
+        [0,3,5],
+        [0,1,4,6],
+        [0,5],
+        [0,1,2,3,4],
+        [0]
+    ]
 
-	def __init__(self,meter):
-		self.meter = meter
+    def __init__(self,meter):
+        self.meter = meter
 
-	def generate(self):
-		"""meter should be a touple """
-		
-		nr_of_bars = random.randint(3,10)	
+    def generate(self):
+        """meter should be a touple """
+        
+        nr_of_bars = random.randint(3,10)	
 
-		diatonic =  self.Diatonic("C")  # example : ['C', 'Db', 'Eb', 'F', 'G', 'Ab', 'Bb']
-		
+        diatonic =  self.Diatonic("C")  # example : ['C', 'Db', 'Eb', 'F', 'G', 'Ab', 'Bb']
+        
 
-		bar = Bar(diatonic[0],self.meter)
+        bar = Bar(diatonic[0],self.meter)
 
-		random_proggresion = [diatonic[0]]
-		nr_of_chords = random.randint(5,15)
-		print diatonic
-		for i in range(1,nr_of_chords):
-			initial_note = random_proggresion[len(random_proggresion)-1]
-			for i in range(0,len(diatonic)):
-				if initial_note == diatonic[i]:
-					print "yeya"
-
-
-			#print self.leads_to[]
-		
-			random_proggresion.append("Bb") 
-		
-			
-		def get_diatonic_index(note):
-			print "hello"	
-			
-			
-
-		
-		
-		return bar
-		
-	def Diatonic(self,note):
-		chord_proggresions = ["major","minor","minor","major","major","minor","minor"]
-		
-		for i in range(0,len(notes)) :
-			if (notes[i] == note) : 
-				diatonic = notes[i:] + notes[:i]
-				
-		for i in range(0,len(notes)) :
-			if chord_proggresions[i] == "minor" :
-				diatonic[i] += "b"
-		return diatonic
-	
+        random_proggresion = [diatonic[0]]
+        nr_of_chords = random.randint(5,15)
+        
+        for i in range(1,nr_of_chords):
+            initial_note = random_proggresion[len(random_proggresion)-1]
+            for i in range(0,len(notes)):
+                if initial_note == notes[i]:
+                    print (GeneratedTrack.leads_to[i])
 
 
+            #print self.leads_to[]
+        
+            random_proggresion.append("Bb") 
+        
+            
+        def get_diatonic_index(note):
+            print ("hello")	
+            
+            
+
+        
+        
+        return bar
+        
+    def Diatonic(self,note):
+        chord_proggresions = ["major","minor","minor","major","major","minor","minor"]
+        
+        for i in range(0,len(notes)) :
+            if (notes[i] == note) : 
+                diatonic = notes[i:] + notes[:i]
+                
+        for i in range(0,len(notes)) :
+            if chord_proggresions[i] == "minor" :
+                diatonic[i] += "b"
+        return diatonic
+    
 
 
 
-	
+
+
+    
 
 
 
